41a339b79b9bd1b45e85d2bd6e90a1c9f7696e2b
Daniel Kennett <daniel@cascable.se>
2021-06-21 15:03:52 +0200

    Update to CascableCore 10.0.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "CascableCore", url: "git@github.com:Cascable/cascablecore-distribution", .exact("10.0.0-rc"))
+        .package(name: "CascableCore", url: "git@github.com:Cascable/cascablecore-distribution", .exact("10.0.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
