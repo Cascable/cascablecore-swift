dd5e59b19604443c0568f3c6284faad0402a9615
Daniel Kennett <daniel@cascable.se>
2021-11-02 13:41:26 +0100

    Update to CascableCore 10.1

--- Package.swift
+++ Package.swift
@@ -1,29 +1,17 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "CascableCoreSwift",
     platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "CascableCoreSwift",
-            targets: ["CascableCoreSwift"]),
+        .library(name: "CascableCoreSwift", targets: ["CascableCoreSwift"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(name: "CascableCore", url: "git@github.com:Cascable/cascablecore-distribution", .exact("10.0.2"))
+        .package(name: "CascableCore", url: "git@github.com:Cascable/cascablecore-distribution", .exact("10.1.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "CascableCoreSwift",
-            dependencies: ["CascableCore"]),
-        .testTarget(
-            name: "CascableCoreSwiftTests",
-            dependencies: ["CascableCoreSwift"]),
+        .target(name: "CascableCoreSwift", dependencies: ["CascableCore"]),
+        .testTarget(name: "CascableCoreSwiftTests", dependencies: ["CascableCoreSwift"])
     ]
 )
