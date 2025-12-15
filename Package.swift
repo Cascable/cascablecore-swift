cf566c17fabe54b3c55f3b9d45b0e91389be9fb4
Daniel Kennett <daniel@cascable.se>
2025-12-15 15:48:52 +0100

    Update for CascableCore 17.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [.macOS(.v11), .iOS(.v14), .macCatalyst(.v15), .visionOS("1.1")],
     products: [.library(name: "CascableCoreSwift", targets: ["CascableCoreSwift"])],
     dependencies: [
-        .package(url: "https://github.com/Cascable/cascablecore-distribution", exact: "17.0.0-beta.3")
+        .package(url: "https://github.com/Cascable/cascablecore-distribution", from: "17.0.0")
     ],
     targets: [
         .target(name: "CascableCoreSwift", dependencies: [.product(name: "CascableCore", package: "cascablecore-distribution")]),
